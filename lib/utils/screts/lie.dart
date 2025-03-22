be96ddb4138854aab7f2c29b97e6daa299f772cb
AMAN KUMAWAT <137716535+AMAN-KUMAWAT@users.noreply.github.com>
2025-03-22 13:21:30 +0530

    Update lie.dart

--- lib/utils/screts/lie.dart
+++ lib/utils/screts/lie.dart
@@ -1,5 +1,5 @@
 class Lie {
-  static final String apiKey = "AIzaSyChLkm55BO5W-HANEWt52SRrfQkozoww8c";
+  static final String apiKey = "mujhe to naan khatai khani h c";
   static final String appId = "1:853796840191:web:05916c48fa6373354ea2ca";
   static final String messageSender = "853796840191";
   static final String projectId = "meetx-45cfb";
